4d9830bbfb51d935458c2ec5288dde73621bd70e
ElrohirGT <elrohirgt@gmail.com>
2019-07-03 16:46:52 -0600

    Estructura Base

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Opera-Minas</title>
+</head>
+<body>
+  <h1>Opera-Minas</h1>
+  <form action="./includes/index.inc.php">
+    <button type="submit" name="submit" value="8">8x8</button>
+    <button type="submit" name="submit" value="10">10x10</button>
+    <button type="submit" name="submit" value="12">12x12</button>
+  </form>
+</body>
+</html>
